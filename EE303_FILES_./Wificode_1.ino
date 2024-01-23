408f065d7b3bb69b6257b14336a995136237dfd7
jahz2323 <38128644+jahz2323@users.noreply.github.com>
2024-01-23 13:56:34 +0000

    Update Wificode_1.ino

--- EE303_FILES_./Wificode_1.ino
+++ EE303_FILES_./Wificode_1.ino
@@ -1,61 +1,79 @@
 #define BUFSIZE 512
-#include <SPI.h>
+#include <SLFS.h>
 #include <WiFi.h>
-//#include <WiFiClient.h>
+#include <WiFiClient.h>
 #include <WiFiServer.h>
+#include <WiFiUdp.h>
+#include <WiFi.h>
+#include <SPI.h>
+
 // wifi details
-char ssid[] = "NETGEAR63";
-char password[] = "littlecello367";
+WiFiClient client;
+char ssid[] = "NETGEAR65";
+char password[] = "freshbird648";
 String postBody;
 String position;
-String r;
-String s;
-int bk=0;
-int cp; //current position
-char server[] = "54.78.246.30";
-int port = 8081;
-WiFiClient client;
+char server[] = "3.250.38.184"; 
+int port = 8000;
+//remote control config 
+int x; 
+int y;
+int z;
+ 
+//responses 
+String _read;
+String _send; 
 
-void connectToWiFi() {
-  Serial.print("Connecting to network: ");
-  Serial.print(ssid);
-  Serial.flush();
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    Serial.flush();
-    delay(300);
-  }
-  Serial.println("Connected");
-  //Serial.print("Obtaining IP address");
-  Serial.flush();
-}  
-
-bool connect() {
-  Serial.println("Connecting to server");
-  if (!client.connect(server, port)) {
-    Serial.println("error connecting to server");
-    return false;
-  }
-  Serial.println("Connected to server");
-  return true;
-}
-
-
-// the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(9600);
   delay(1000);
   connectToWiFi();
-  postBody="position=";
-  position="0";
-  Serial.println(position);
+  // send post request and headers
+  postBody = "position=";
+  position = "0";
   postBody += position;
-  Serial.println(postBody);
+
+// send post body
+  client.println(postBody);
 }
 
-// read buffer size for HTTP response
+void loop(){
+  
+  client.println("POST /api/arrived/kltg2668/1.1");  
+  client.println("Content-Type: application/x-www-form-urlencoded");
+  client.print("Content-Length: ");
+  client.println(postBody.length());
+  client.println("Connection: close");
+  client.println();
+
+   // send post body
+   client.println(postBody);
+   _read = readResponse();
+   _send = getResponseBody(_read);
+   Serial.println(_send);
+   client.stop();
+   position = _send;
+   postBody = "position=";
+   postBody += position;
+   if(position!="undefined"){
+    // normal function
+  }
+  else{
+    // stop code
+    while (1) {}
+    }
+  }
+
+bool connect() {
+  Serial.println("Connecting to server");
+    if (!client.connect(server, port)) {
+    Serial.println("error connecting to server");
+  return false;
+    }
+    Serial.println("Connected to server");
+    return true;
+}
+
 String readResponse() {
   char buffer[BUFSIZE];
   memset(buffer, 0, BUFSIZE);
@@ -63,12 +81,6 @@ String readResponse() {
   String response(buffer);
   return response;
 }
-  
-int getStatusCode(String& response) {
-  String code = response.substring(9, 12);
-  return code.toInt();
-}
-    
 String getResponseBody(String& response) {
   int split = response.indexOf("\r\n\r\n");
   String body = response.substring(split+4, response.length());
@@ -76,35 +88,26 @@ String getResponseBody(String& response) {
   return body;
 }
 
-// the loop routine runs over and over again continuously:
-void loop() {
-  //my code
-  connect();
-  client.println("POST /api/arrived/TEAMCODE HTTP/1.1");
-  client.println("Content-Type: application/x-www-form-urlencoded");
-  client.print("Content-Length: ");
-  client.println(postBody.length());
-  client.println("Connection: close");
-  client.println();
+void connectToWiFi() {
+  Serial.print("Connecting to network: ");
+  Serial.print(ssid);
+  Serial.flush();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    Serial.flush();
+    delay(300);
+  }
+  Serial.println("Connected");
+  Serial.print("Obtaining IP address");
+  Serial.flush();
 
-  // send post body
-  client.println(postBody);
-  r = readResponse();
-  //getStatusCode(r);
-  s = getResponseBody(r);
-  Serial.println(s);
-  client.stop();
-  position = s;
-  postBody = "position=";
-  postBody += position;
-  Serial.println(postBody);
-  cp=position.toInt();
-  
-  if(position!="undefined"){
-    // normal function
-  }
-  else{
-    // stop code
-    while (1) {}
-  }
+  while (WiFi.localIP() == INADDR_NONE) {
+  Serial.print(".");
+  Serial.flush();
+  delay(300);
+}
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
