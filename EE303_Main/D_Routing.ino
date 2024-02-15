335b9231257857cc20bf0de1576af3db0ef7d483
jahz2323 <38128644+jahz2323@users.noreply.github.com>
2024-02-15 12:57:25 +0000

    Update D_Routing.ino

--- EE303_Main/D_Routing.ino
+++ EE303_Main/D_Routing.ino
@@ -1,8 +1,10 @@
 int previous = 6;
 int current = 0;
 int junctionCount = 0;
-int destination = 0;
+int destination = 111;
 
+
+int distance = 0;
 
 void setupdecehlo() {
   // put your setup code here, to run once
@@ -15,28 +17,30 @@ void setupdecehlo() {
 }
 
 void loopdechelo() {
-
+  distance = analogRead(A5);
+  //  Serial.println("   ");
+  //  Serial.print(distance);
 }
 
 void junction() {
   junctionCount++;
-  Serial.print("  NUM OF JUNCTIONS     ");
-  Serial.println(junctionCount);
-  Serial.println(" ");
-  Serial.print("  Previous Position is.... ");
-  Serial.print(previous);
-  Serial.print("   Current Position is...  ");
-  Serial.print(current); 
-  Serial.println(" ");
-  Serial.print("  Destination Position is...  ");
-  Serial.print(destination); 
-  Serial.println(" ");
   if (junctionCount == 1) {
     post();
   }
   else {
     test(destination);
   }
+  Serial.print("  NUM OF JUNCTIONS     ");
+  Serial.println(junctionCount);
+  Serial.println(" ");
+  Serial.print("  Previous Position is.... ");
+  Serial.print(previous);
+  Serial.print("   Current Position is...  ");
+  Serial.print(current);
+  Serial.println(" ");
+  Serial.print("  Destination Position is...  ");
+  Serial.print(destination);
+  Serial.println(" ");
 }
 
 void post() {
@@ -44,61 +48,38 @@ void post() {
     POST_position(0);
     delay(500);
     destination = Read_Response();
-    Serial.print("     Destination is...");
-    Serial.print(destination);
-
+    if (destination == 4) {
+      spin_turn();
+    }
   }
 }
 
-  //spin_turn(){
-  //left_angle_turn() {
-  //right_angle_turn() {
-  //move_to_five() {
-  bool test(int dest) {
-    //FROM 0
-    if ( (previous == 6) && (current == 0) && (dest == 1)) {
-      if (junctionCount = 2) {
-        Serial.println(" left");
-        left_angle_turn();
-        //left turn
-      }
-      if (junctionCount = 3) {
-        Serial.println("FINISHED");
-        POST_position(1);
-        current == 1;
-        junctionCount == 0;
-        return true;
-      }
+bool test(int dest) {
+  //FROM ZERO
+  if ( previous == 6 && current == 0 && destination == 1) {
+    if (junctionCount == 2) {
+      Serial.println(" left");
+      left_angle_turn();
+      //left turn
     }
-    else if ( (previous == 6) && (current == 0) && (dest == 4) ) {
-      if (junctionCount == 1) {
-        spin_turn();
-      }
-      if (junctionCount == 2) {
-        POST_position(4);
-        current == 4;
-        junctionCount == 0;
-        return true;
-      }
+    if (junctionCount == 3) {
+      //Serial.println("FINISHED");
+      previous = 0;
+      current = 1;
+      //    junctionCount == 0;
+      POST_position(1);
+      delay(500);
+      destination = Read_Response();
+      return true;
     }
-    else if ( (previous == 6) && (current == 0) && (dest == 2)) {
-      if (junctionCount == 3) {
-        POST_position(2);
-        current == 2;
-        junctionCount == 0;
-        return true;
-      }
-    }
-    else if ( (previous == 6) && (current == 0) && (dest == 3)) {
-      if (junctionCount == 4) {
-        POST_position(3);
-        current == 3;
-        junctionCount == 0;
-        return true;
-      }
-    }
-    //
-    //
-    //
-    return false;
   }
+  else if ( previous == 0 && current == 1 && destination == 5) {
+
+    if (distance > 900) {
+      POST_position(5);
+    }
+    return true;
+  }
+  return false;
+}
+
