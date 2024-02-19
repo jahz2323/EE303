6d726eebc46ebd297ac94f8c6c961bda56789272
jahz2323 <38128644+jahz2323@users.noreply.github.com>
2024-02-19 19:55:25 +0000

    Update C_Motor.ino

--- EE303_Main/C_Motor.ino
+++ EE303_Main/C_Motor.ino
@@ -8,21 +8,21 @@ bool motor_flag;
 // the setup routine runs once when you press reset:
 void setupC_Motor()
 { 
-  Serial.begin(9600);
+  Serial.begin(9700);
   delay(10000);
 }
 // the loop routine runs over and over again continuously:
 void loopC_Motor()
   {
-    if ((AnalogValue[2] < 600) && (AnalogValue[0] > 600) && (AnalogValue[4] > 600)) {
+    if ((AnalogValue[2] < 700) && (AnalogValue[0] > 700) && (AnalogValue[4] > 700)) {
       digitalWrite(motor1Phase, HIGH); //forward
       digitalWrite(motor2Phase, HIGH); //forward
-      analogWrite(motor1PWM, 180); // set speed of motor
-      analogWrite(motor2PWM, 180); // set speed of motor
+      analogWrite(motor1PWM, 200); // set speed of motor
+      analogWrite(motor2PWM, 200); // set speed of motor
 
     }
     //REACHED STATION
-    else if ( (AnalogValue[0] < 600) && (AnalogValue[2] < 600) && (AnalogValue[4] < 600)) {
+    else if ( (AnalogValue[0] < 700) && (AnalogValue[2] < 700) && (AnalogValue[4] < 700)) {
       digitalWrite(motor1Phase, HIGH); //forward
       digitalWrite(motor2Phase, HIGH); //forward
       analogWrite(motor1PWM, 0); // set speed of motor
@@ -34,31 +34,31 @@ void loopC_Motor()
       analogWrite(motor2PWM, 70); // set speed of motor
       delay(300);
     }
-    else if (AnalogValue[3] < 600) {
+    else if (AnalogValue[3] < 700) {
       digitalWrite(motor1Phase, HIGH); //forward
       digitalWrite(motor2Phase, HIGH); //forward
-      analogWrite(motor1PWM, 70); // set speed of motor
-      analogWrite(motor2PWM, 150); // set speed of motor
+      analogWrite(motor1PWM, 90); // set speed of motor
+      analogWrite(motor2PWM, 170); // set speed of motor
      // Serial.println("RIGHT"); // Display motor direction
     }
-    else if (AnalogValue[4] < 600) {
+    else if (AnalogValue[4] < 700) {
       digitalWrite(motor1Phase, HIGH); //forward
       digitalWrite(motor2Phase, HIGH); //forward
       analogWrite(motor1PWM, 50); // set speed of motor
-      analogWrite(motor2PWM, 180); // set speed of motor
+      analogWrite(motor2PWM, 200); // set speed of motor
       //Serial.println("RIGHT"); // Display motor direction
     }
-    else if (AnalogValue[1] < 600) {
+    else if (AnalogValue[1] < 700) {
       digitalWrite(motor1Phase, HIGH); //forward
       digitalWrite(motor2Phase, HIGH); //forward
-      analogWrite(motor1PWM, 150); // set speed of motor
-      analogWrite(motor2PWM, 70); // set speed of motor
+      analogWrite(motor1PWM, 170); // set speed of motor
+      analogWrite(motor2PWM, 90); // set speed of motor
      // Serial.println("LEFT"); // Display motor direction
     }
-    else if (AnalogValue[0] < 600) {
+    else if (AnalogValue[0] < 700) {
       digitalWrite(motor1Phase, HIGH); //forward
       digitalWrite(motor2Phase, HIGH); //forward
-      analogWrite(motor1PWM, 180); // set speed of motor
+      analogWrite(motor1PWM, 200); // set speed of motor
       analogWrite(motor2PWM, 50); // set speed of motor
      // Serial.println("LEFT"); // Display motor direction
     }
