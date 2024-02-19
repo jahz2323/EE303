467555541e703690a092549cb0803d5684e00e9d
Lucas <122633837+Lobonho@users.noreply.github.com>
2024-02-19 17:29:55 +0000

    Update E_Manual.ino

--- EE303_Main/E_Manual.ino
+++ EE303_Main/E_Manual.ino
@@ -1,41 +1,75 @@
 void setupE_Manual() {
-  
+
 }
 
 void loopE_Manual() {
-  
+
 }
 
-void spin_turn(){
-      digitalWrite(motor1Phase, LOW); //forward
-      digitalWrite(motor2Phase, HIGH); //forward
-      analogWrite(motor1PWM, 175); // set speed of motor
-      analogWrite(motor2PWM, 175); // set speed of motor
-      delay(250);
+void spin_turn() {
+  digitalWrite(motor1Phase, LOW); //forward
+  digitalWrite(motor2Phase, HIGH); //forward
+  analogWrite(motor1PWM, 175); // set speed of motor
+  analogWrite(motor2PWM, 175); // set speed of motor
+  delay(250);
 }
 
 void left_angle_turn() {
-      digitalWrite(motor1Phase, HIGH); //forward
-      digitalWrite(motor2Phase, HIGH); //forward
-      analogWrite(motor1PWM, 150); // set speed of motor
-      analogWrite(motor2PWM, 0); // set speed of motor
-      delay(100);
-   
+  digitalWrite(motor1Phase, HIGH); //forward
+  digitalWrite(motor2Phase, HIGH); //forward
+  analogWrite(motor1PWM, 150); // set speed of motor
+  analogWrite(motor2PWM, 0); // set speed of motor
+  delay(100);
+
 }
 
 void right_angle_turn() {
-      digitalWrite(motor1Phase, HIGH); //forward
-      digitalWrite(motor2Phase, HIGH); //forward
-      analogWrite(motor1PWM, 0); // set speed of motor
-      analogWrite(motor2PWM, 150); // set speed of motor
-      delay(100);
- 
+  digitalWrite(motor1Phase, HIGH); //forward
+  digitalWrite(motor2Phase, HIGH); //forward
+  analogWrite(motor1PWM, 0); // set speed of motor
+  analogWrite(motor2PWM, 150); // set speed of motor
+  delay(100);
+
+}
+void stopmotor() {
+  digitalWrite(motor1Phase, HIGH); //forward
+  digitalWrite(motor2Phase, HIGH); //forward
+  analogWrite(motor1PWM, 0); // set speed of motor
+  analogWrite(motor2PWM, 0); // set speed of motor
+  delay(10000);
 }
 
-void park() {
-      digitalWrite(motor1Phase, HIGH); //forward
-      digitalWrite(motor2Phase, HIGH); //forward
-      analogWrite(motor1PWM, 185); // set speed of motor
-      analogWrite(motor2PWM, 185); // set speed of motor
-      delay(4000);
+
+
+bool park() {
+  distance = analogRead(A5);
+  Serial.print(distance);
+  Serial.println("   ");
+  Serial.print("\t"); //tab over on screen
+  //  digitalWrite(motor1Phase, HIGH); //forward
+  //  digitalWrite(motor2Phase, HIGH); //forward
+  //  analogWrite(motor1PWM, 185); // set speed of motor
+  //  analogWrite(motor2PWM, 185); // set speed of motor
+  //  delay(4000);
+  if (distance >= 0 && distance < 200) {
+    Serial.println(" KEEEP FUCKING MOVING");
+    analogWrite(motor1PWM, 230); // set speed of motor
+    analogWrite(motor2PWM, 230); // set speed of motor
+    park();
+  }
+
+  else if (distance > 200 && distance < 800) {
+    Serial.println(" stopping...");
+    analogWrite(motor1PWM, 120); // set speed of motor
+    analogWrite(motor2PWM, 120); // set speed of motor
+    park();
+  }
+  else if (distance > 800) {
+    Serial.println(" FUCKING STOP");
+    analogWrite(motor1PWM, 0); // set speed of motor
+    analogWrite(motor2PWM, 0); // set speed of motor
+    return false;
+
+  }
+  return true;
 }
